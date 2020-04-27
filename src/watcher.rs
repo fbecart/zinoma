da52a9d5eee07da7bbf01bcd6bf4ce574634520d
Florent Bécart <florent.becart@gmail.com>
2020-04-27 14:19:06 +0300

    Fix issue when watching temporary files

--- src/watcher.rs
+++ src/watcher.rs
@@ -1,6 +1,7 @@
 use crate::target::{Target, TargetId};
 use crossbeam::channel::{unbounded, Receiver, TryRecvError};
 use notify::{RawEvent, RecommendedWatcher, RecursiveMode, Watcher};
+use std::path::Path;
 
 pub struct TargetsWatcher {
     target_watchers: Vec<TargetWatcher>,
@@ -60,9 +61,52 @@ impl TargetWatcher {
 
     pub fn is_invalidated(&self) -> Result<bool, String> {
         match self.rx.try_recv() {
-            Ok(_) => Ok(true),
+            Ok(event) => {
+                let path = event.path.unwrap();
+                Ok(!is_tmp_editor_file(&path))
+            }
             Err(TryRecvError::Empty) => Ok(false),
             Err(e) => Err(format!("Watcher received error: {}", e)),
         }
+    }
+}
+
+fn is_tmp_editor_file(file_path: &Path) -> bool {
+    let file_name = file_path.file_name().unwrap();
+    let file_name = file_name.to_str().unwrap();
+
+    if file_name.ends_with('~') {
+        return true; // IntelliJ IDEA
+    }
+
+    if file_name.starts_with('.') && (file_name.ends_with(".swp") || file_name.ends_with(".swx")) {
+        return true; // Vim
+    }
+
+    false
+}
+
+#[cfg(test)]
+mod is_tmp_editor_file_tests {
+    use super::is_tmp_editor_file;
+    use std::path::Path;
+
+    #[test]
+    fn src_file_should_not_be_tmp_editor_file() {
+        assert!(!is_tmp_editor_file(Path::new("/my/project/src/main.rs")));
+    }
+
+    #[test]
+    fn intellij_tmp_file_should_be_tmp_editor_file() {
+        assert!(is_tmp_editor_file(Path::new("/my/project/src/main.rs~")));
+    }
+
+    #[test]
+    fn vim_tmp_file_should_be_tmp_editor_file() {
+        let path = Path::new("/my/project/src/.main.rs.swp");
+        assert!(is_tmp_editor_file(path));
+
+        let path = Path::new("/my/project/src/.main.rs.swx");
+        assert!(is_tmp_editor_file(path));
     }
 }
