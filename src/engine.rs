0c93e70d82b1789e7bcc38aa989ddbecf95d1612
Florent Bécart <florent.becart@gmail.com>
2020-04-27 20:26:08 +0300

    Refactor engine

--- src/engine.rs
+++ src/engine.rs
@@ -49,9 +49,9 @@ impl<'a> Engine<'a> {
                 for &target_id in self.get_ready_to_build_targets(&target_build_states).iter() {
                     let target = self.targets.get(target_id).unwrap();
                     target_build_states[target.id].build_started();
-                    let tx_clone = tx.clone();
+                    let tx = tx.clone();
                     scope.spawn(move |_| {
-                        self.build_target(target.id, tx_clone)
+                        build_target(target, &self.incremental_runner, &tx)
                             .map_err(|e| format!("Error building target {}: {}", target.id, e))
                             .unwrap()
                     });
@@ -68,7 +68,7 @@ impl<'a> Engine<'a> {
                         } else {
                             target_build_states[target_id].build_succeeded();
 
-                            if let Some(command) = &target.service {
+                            if target.service.is_some() {
                                 // If already running, send a kill signal.
                                 if let Some(service_tx) = &service_tx_channels[target_id] {
                                     service_tx.send(RunSignal::Kill).map_err(|e| {
@@ -83,8 +83,7 @@ impl<'a> Engine<'a> {
                                 service_tx_channels[target_id] = Some(service_tx);
 
                                 scope.spawn(move |_| {
-                                    self.run_target_service(target, command, service_rx)
-                                        .unwrap()
+                                    run_target_service(target, service_rx).unwrap()
                                 });
                             }
                         }
@@ -124,9 +123,9 @@ impl<'a> Engine<'a> {
             for &target_id in self.get_ready_to_build_targets(&target_build_states).iter() {
                 let target = self.targets.get(target_id).unwrap();
                 target_build_states[target.id].build_started();
-                let tx_clone = tx.clone();
+                let tx = tx.clone();
                 scope.spawn(move |_| {
-                    self.build_target(target.id, tx_clone)
+                    build_target(target, &self.incremental_runner, &tx)
                         .map_err(|e| format!("Error building target {}: {}", target.id, e))
                         .unwrap()
                 });
@@ -149,84 +148,6 @@ impl<'a> Engine<'a> {
             sleep(Duration::from_millis(10))
         })
         .map_err(|_| "Unknown crossbeam parallelism failure (thread panicked)".to_string())?
-    }
-
-    fn build_target(&self, target_id: TargetId, tx: Sender<BuildResult>) -> Result<(), String> {
-        let target = self.targets.get(target_id).unwrap();
-        let incremental_run_result: IncrementalRunResult<Result<(), String>> = self
-            .incremental_runner
-            .run(&target.name, &target.watch_list, || {
-                let target_start = Instant::now();
-                log::info!("{} - Building", &target.name);
-                for command in target.build_list.iter() {
-                    let command_start = Instant::now();
-                    log::debug!("{} - Command \"{}\" - Executing", target.name, command);
-                    let command_output = cmd!("/bin/sh", "-c", command)
-                        .dir(&target.path)
-                        .stderr_to_stdout()
-                        .run()
-                        .map_err(|e| format!("Command execution error: {}", e))?;
-                    print!(
-                        "{}",
-                        String::from_utf8(command_output.stdout)
-                            .map_err(|e| format!("Failed to interpret stdout as utf-8: {}", e))?
-                    );
-                    let command_execution_duration = command_start.elapsed();
-                    log::debug!(
-                        "{} - Command \"{}\" - Success (took: {}ms)",
-                        target.name,
-                        command,
-                        command_execution_duration.as_millis()
-                    );
-                }
-                let target_build_duration = target_start.elapsed();
-                log::info!(
-                    "{} - Built (took: {}ms)",
-                    target.name,
-                    target_build_duration.as_millis()
-                );
-                Ok(())
-            })
-            .map_err(|e| format!("Incremental build error: {}", e))?;
-
-        if incremental_run_result == IncrementalRunResult::Skipped {
-            log::info!("{} - Build skipped (Not Modified)", target.name);
-        }
-
-        let build_result_state = match incremental_run_result {
-            IncrementalRunResult::Skipped => BuildResultState::Skip,
-            IncrementalRunResult::Run(Ok(_)) => BuildResultState::Success,
-            IncrementalRunResult::Run(Err(e)) => BuildResultState::Fail(e),
-        };
-        tx.send(BuildResult {
-            target_id: target.id,
-            state: build_result_state,
-        })
-        .map_err(|e| format!("Sender error: {}", e))
-    }
-
-    fn run_target_service(
-        &self,
-        target: &Target,
-        command: &str,
-        rx: Receiver<RunSignal>,
-    ) -> Result<(), String> {
-        log::info!("{} - Command: \"{}\" - Run", target.name, command);
-        let handle = cmd!("/bin/sh", "-c", command)
-            .dir(&target.path)
-            .stderr_to_stdout()
-            .start()
-            .map_err(|e| format!("Failed to run command {}: {}", command, e))?;
-
-        match rx.recv() {
-            Ok(RunSignal::Kill) => {
-                log::trace!("{} - Killing process", target.name);
-                handle
-                    .kill()
-                    .map_err(|e| format!("Failed to kill process {}: {}", command, e))
-            }
-            Err(e) => Err(format!("Receiver error: {}", e)),
-        }
     }
 
     fn get_ready_to_build_targets(
@@ -254,6 +175,85 @@ impl<'a> Engine<'a> {
                 && self.has_all_dependencies_built(dependency_id, target_build_states)
         })
     }
+}
+
+fn build_target(
+    target: &Target,
+    incremental_runner: &IncrementalRunner,
+    tx: &Sender<BuildResult>,
+) -> Result<(), String> {
+    let incremental_run_result = incremental_runner
+        .run(&target.name, &target.watch_list, || {
+            let target_start = Instant::now();
+            log::info!("{} - Building", &target.name);
+            for command in target.build_list.iter() {
+                let command_start = Instant::now();
+                log::debug!("{} - Command \"{}\" - Executing", target.name, command);
+                let command_output = cmd!("/bin/sh", "-c", command)
+                    .dir(&target.path)
+                    .stderr_to_stdout()
+                    .run()
+                    .map_err(|e| format!("Command execution error: {}", e))?;
+                print!(
+                    "{}",
+                    String::from_utf8(command_output.stdout)
+                        .map_err(|e| format!("Failed to interpret stdout as utf-8: {}", e))?
+                );
+                let command_execution_duration = command_start.elapsed();
+                log::debug!(
+                    "{} - Command \"{}\" - Success (took: {}ms)",
+                    target.name,
+                    command,
+                    command_execution_duration.as_millis()
+                );
+            }
+            let target_build_duration = target_start.elapsed();
+            log::info!(
+                "{} - Built (took: {}ms)",
+                target.name,
+                target_build_duration.as_millis()
+            );
+            Ok(())
+        })
+        .map_err(|e| format!("Incremental build error: {}", e))?;
+
+    if incremental_run_result == IncrementalRunResult::Skipped {
+        log::info!("{} - Build skipped (Not Modified)", target.name);
+    }
+
+    let build_result_state = match incremental_run_result {
+        IncrementalRunResult::Skipped => BuildResultState::Skip,
+        IncrementalRunResult::Run(Ok(_)) => BuildResultState::Success,
+        IncrementalRunResult::Run(Err(e)) => BuildResultState::Fail(e),
+    };
+    tx.send(BuildResult {
+        target_id: target.id,
+        state: build_result_state,
+    })
+    .map_err(|e| format!("Sender error: {}", e))
+}
+
+fn run_target_service(target: &Target, rx: Receiver<RunSignal>) -> Result<(), String> {
+    if let Some(command) = &target.service {
+        log::info!("{} - Command: \"{}\" - Run", target.name, command);
+        let handle = cmd!("/bin/sh", "-c", command)
+            .dir(&target.path)
+            .stderr_to_stdout()
+            .start()
+            .map_err(|e| format!("Failed to run command {}: {}", command, e))?;
+
+        match rx.recv() {
+            Ok(RunSignal::Kill) => {
+                log::trace!("{} - Killing process", target.name);
+                handle
+                    .kill()
+                    .map_err(|e| format!("Failed to kill process {}: {}", command, e))
+            }
+            Err(e) => Err(format!("Receiver error: {}", e)),
+        }?
+    }
+
+    Ok(())
 }
 
 #[derive(Clone)]
