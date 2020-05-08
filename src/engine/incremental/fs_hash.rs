2fbb30642ae4906d26dee0b1eb09f38711aa55f6
Florent Bécart <florent.becart@gmail.com>
2020-05-08 11:39:41 +0300

    Use a buffered reader to compute file hash

--- src/engine/incremental/fs_hash.rs
+++ src/engine/incremental/fs_hash.rs
@@ -2,6 +2,7 @@ use anyhow::{Context, Result};
 use seahash::SeaHasher;
 use std::fs;
 use std::hash::Hasher;
+use std::io::{BufReader, Read};
 use std::path::{Path, PathBuf};
 use std::time::Instant;
 use walkdir::WalkDir;
@@ -16,8 +17,8 @@ pub fn compute_paths_hash(identifier: &str, paths: &[PathBuf]) -> Result<Option<
     let mut hasher = SeaHasher::default();
 
     for path in paths.iter() {
-        let checksum = calculate_path_hash(path)?;
-        Hasher::write_u64(&mut hasher, checksum.unwrap_or(0));
+        let path_hash = compute_path_hash(path)?;
+        Hasher::write_u64(&mut hasher, path_hash.unwrap_or(0));
     }
 
     let computation_duration = computation_start.elapsed();
@@ -30,7 +31,7 @@ pub fn compute_paths_hash(identifier: &str, paths: &[PathBuf]) -> Result<Option<
     Ok(Some(hasher.finish()))
 }
 
-fn calculate_path_hash(path: &Path) -> Result<Option<u64>> {
+fn compute_path_hash(path: &Path) -> Result<Option<u64>> {
     if !path.exists() {
         return Ok(None);
     }
@@ -39,13 +40,31 @@ fn calculate_path_hash(path: &Path) -> Result<Option<u64>> {
 
     for entry in WalkDir::new(path) {
         let entry = entry.with_context(|| "Failed to traverse directory")?;
-
-        if entry.path().is_file() {
-            let contents = fs::read(entry.path())
-                .with_context(|| "Failed to read file to calculate checksum")?;
-            Hasher::write(&mut hasher, &contents);
+        let path = entry.path();
+        if path.is_file() {
+            let file_hash = compute_file_hash(path)
+                .with_context(|| format!("Failed to compute hash of file {}", path.display()))?;
+            Hasher::write_u64(&mut hasher, file_hash);
         }
     }
 
     Ok(Some(hasher.finish()))
+}
+
+fn compute_file_hash(file_path: &Path) -> Result<u64> {
+    let mut hasher = SeaHasher::default();
+    let file = fs::File::open(file_path)
+        .with_context(|| format!("Failed to open file {}", file_path.display()))?;
+    let mut reader = BufReader::new(file);
+
+    let mut buffer = [0; 1024];
+    loop {
+        let count = reader.read(&mut buffer)?;
+        if count == 0 {
+            break;
+        }
+        Hasher::write(&mut hasher, &buffer[..count]);
+    }
+
+    Ok(hasher.finish())
 }
