049baa68cc7e44f681b0b74c658a277771200892
Florent Bécart <florent.becart@gmail.com>
2020-06-04 16:25:58 +0300

    Remove 'dbg!'

--- src/engine/incremental/resources_state/cmd_stdout.rs
+++ src/engine/incremental/resources_state/cmd_stdout.rs
@@ -18,14 +18,13 @@ impl ResourcesState {
     }
 
     pub fn eq_current_state(&self, cmds: &[String], dir: &Path) -> bool {
-        cmds.par_iter()
-            .all(|cmd| match dbg!(get_cmd_stdout(cmd, dir)) {
-                Ok(stdout) => dbg!(self.0.get(cmd)) == Some(&stdout),
-                Err(e) => {
-                    log::error!("Command {} failed to execute: {}", cmd, e);
-                    false
-                }
-            })
+        cmds.par_iter().all(|cmd| match get_cmd_stdout(cmd, dir) {
+            Ok(stdout) => self.0.get(cmd) == Some(&stdout),
+            Err(e) => {
+                log::error!("Command {} failed to execute: {}", cmd, e);
+                false
+            }
+        })
     }
 }
 
