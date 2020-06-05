73bfdc2f6a060a4e094def4b2a85cfe681e5756e
fbecart <florent.becart@gmail.com>
2020-06-05 14:00:40 +0000

    Deploying to gh-pages from  @ 50edbe1b4ba74bfed0a97023911fea26d38b27f9 🚀

--- doc/zinoma/sidebar-items.js
+++ doc/zinoma/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["main",""],["terminate_on_ctrlc",""]],"mod":[["clean",""],["cli",""],["config",""],["domain",""],["engine",""],["work_dir",""]],"static":[["GLOBAL",""]]});
+initSidebarItems({"fn":[["main",""],["terminate_on_ctrlc",""]],"mod":[["clean",""],["cli",""],["config",""],["domain",""],["engine",""],["run_script",""],["work_dir",""]],"static":[["GLOBAL",""]]});
