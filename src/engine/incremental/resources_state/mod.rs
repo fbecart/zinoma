51b3e38dd19b899fb9882c4a69937efd6ec13c4c
Florent Bécart <florent.becart@gmail.com>
2020-06-26 14:20:07 +0300

    Async incremental mod

--- src/engine/incremental/resources_state/mod.rs
+++ src/engine/incremental/resources_state/mod.rs
@@ -3,6 +3,7 @@ mod fs;
 
 use crate::domain::Resources;
 use anyhow::Result;
+use futures::future;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq)]
@@ -13,15 +14,26 @@ pub struct ResourcesState {
 
 impl ResourcesState {
     pub async fn current(resources: &Resources) -> Result<Self> {
+        let (fs, cmd_stdout) = future::join(
+            fs::ResourcesState::current(&resources.paths),
+            cmd_stdout::ResourcesState::current(&resources.cmds),
+        )
+        .await;
+
         Ok(Self {
-            fs: fs::ResourcesState::current(&resources.paths).await?,
-            cmd_stdout: cmd_stdout::ResourcesState::current(&resources.cmds).await?,
+            fs: fs?,
+            cmd_stdout: cmd_stdout?,
         })
     }
 
     pub async fn eq_current_state(&self, resources: &Resources) -> Result<bool> {
-        // TODO Parallelize this computation
-        Ok((&self.fs).eq_current_state(&resources.paths).await
-            && (&self.cmd_stdout).eq_current_state(&resources.cmds).await)
+        // TODO Resolve at the first negative result
+        let (fs, cmd_stdout) = future::join(
+            self.fs.eq_current_state(&resources.paths),
+            self.cmd_stdout.eq_current_state(&resources.cmds),
+        )
+        .await;
+
+        Ok(fs && cmd_stdout)
     }
 }
