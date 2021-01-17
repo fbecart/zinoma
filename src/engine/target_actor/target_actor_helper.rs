fbecart/zinoma

commit ddb3fd6b6b44f12080f0b3a75976b403423e18de

Update dependency async_std

src/engine/target_actor/target_actor_helper.rs
@@ -3,7 +3,7 @@ use crate::domain::{TargetId, TargetMetadata};
 use crate::engine::watcher::TargetInvalidatedMessage;
 use crate::TerminationMessage;
 use anyhow::Error;
-use async_std::sync::{Receiver, Sender};
+use async_std::channel::{Receiver, Sender};
 use std::collections::{HashMap, HashSet};
 
 pub struct TargetActorHelper {
@@ -78,13 +78,14 @@ impl TargetActorHelper {
     pub async fn notify_execution_failed(&mut self, e: Error) {
         self.executed = false;
         let msg = TargetActorOutputMessage::TargetExecutionError(self.target_id.clone(), e);
-        self.target_actor_output_sender.send(msg).await;
+        self.target_actor_output_sender.send(msg).await.unwrap()
     }
 
     pub async fn send_to_actor(&self, dest: ActorId, msg: ActorInputMessage) {
         self.target_actor_output_sender
             .send(TargetActorOutputMessage::MessageActor { dest, msg })
             .await
+            .unwrap()
     }
 
     pub async fn send_to_dependencies(&self, msg: ActorInputMessage) {

src/engine/target_actors.rs
@@ -5,11 +5,10 @@ use super::WatchOption;
 use crate::domain::{Target, TargetId};
 use crate::TerminationMessage;
 use anyhow::Result;
-use async_std::sync;
+use async_std::channel::Sender;
 use async_std::task::JoinHandle;
 use futures::future;
 use std::collections::HashMap;
-use sync::Sender;
 
 pub struct TargetActors {
     targets: HashMap<TargetId, Target>,
@@ -56,7 +55,8 @@ impl TargetActors {
         self.get_target_actor_handles(target_id)?
             .target_actor_input_sender
             .send(msg)
-            .await;
+            .await
+            .unwrap();
 
         Ok(())
     }
@@ -68,7 +68,11 @@ impl TargetActors {
                 kind,
                 requester: ActorId::Root,
             };
-            handles.target_actor_input_sender.send(build_msg).await;
+            handles
+                .target_actor_input_sender
+                .send(build_msg)
+                .await
+                .unwrap();
         }
 
         Ok(())
@@ -84,7 +88,11 @@ impl TargetActors {
     ) {
         log::debug!("Terminating all targets");
         for handles in target_actor_handles.values() {
-            handles.termination_sender.send(TerminationMessage).await;
+            handles
+                .termination_sender
+                .send(TerminationMessage)
+                .await
+                .unwrap();
         }
     }
 }

src/main.rs
@@ -12,8 +12,8 @@ mod work_dir;
 
 use anyhow::{Context, Result};
 use async_ctrlc::CtrlC;
+use async_std::channel::{self, Receiver};
 use async_std::path::PathBuf;
-use async_std::sync::{self, Receiver};
 use async_std::task;
 use clean::clean_target_output_paths;
 use config::{ir, yaml};
@@ -92,7 +92,7 @@ fn main() -> Result<()> {
             let termination_events = terminate_on_ctrlc()?;
 
             let (target_actor_output_sender, target_actor_output_events) =
-                sync::channel(crate::DEFAULT_CHANNEL_CAP);
+                channel::bounded(crate::DEFAULT_CHANNEL_CAP);
             let mut target_actors =
                 TargetActors::new(targets, target_actor_output_sender, watch_option);
 
@@ -115,13 +115,13 @@ fn main() -> Result<()> {
 }
 
 fn terminate_on_ctrlc() -> Result<Receiver<TerminationMessage>> {
-    let (termination_sender, termination_events) = sync::channel(1);
+    let (termination_sender, termination_events) = channel::bounded(1);
     let ctrlc = CtrlC::new().with_context(|| "Failed to set Ctrl-C handler")?;
 
     task::spawn(async move {
         ctrlc.await;
         log::debug!("Ctrl-C received");
-        termination_sender.send(TerminationMessage).await;
+        termination_sender.send(TerminationMessage).await.unwrap();
     });
 
     Ok(termination_events)

Cargo.toml
@@ -31,7 +31,7 @@ async-ctrlc = { version = "1.2.0", features = ["termination"] }
 schemars = "0.8"
 dunce = "1.0"
 itertools = "0.10"
-async-std = { version = "1.6", features = ["unstable"] }
+async-std = { version = "1.9", features = ["unstable"] }
 futures = "0.3"
 pin-project-lite = "0.1"
 async-process = "1.0"

src/engine/builder.rs
@@ -1,8 +1,8 @@
 use crate::domain::BuildTarget;
 use crate::run_script;
 use anyhow::{anyhow, Context, Result};
+use async_std::channel::Receiver;
 use async_std::prelude::*;
-use async_std::sync::Receiver;
 use futures::FutureExt;
 use std::process::Stdio;
 use std::time::Instant;

src/engine/mod.rs
@@ -7,8 +7,8 @@ mod watcher;
 use crate::domain::TargetId;
 use crate::TerminationMessage;
 use anyhow::{Context, Result};
+use async_std::channel::Receiver;
 use async_std::prelude::*;
-use async_std::sync::Receiver;
 use futures::FutureExt;
 use std::collections::HashSet;
 use target_actor::{ActorId, ActorInputMessage, ExecutionKind, TargetActorOutputMessage};

src/engine/target_actor/build_target_actor.rs
@@ -1,7 +1,7 @@
 use super::{ActorInputMessage, ExecutionKind, TargetActorHelper};
 use crate::domain::BuildTarget;
 use crate::engine::{builder, incremental};
-use async_std::{prelude::*, sync};
+use async_std::{channel, prelude::*};
 use builder::BuildCancellationMessage;
 use futures::future::Fuse;
 use futures::{pin_mut, FutureExt};
@@ -30,7 +30,7 @@ impl BuildTargetActor {
             if self.helper.should_execute(ExecutionKind::Build)
                 && ongoing_build_cancellation_sender.is_none()
             {
-                let (build_cancellation_sender, build_cancellation_events) = sync::channel(1);
+                let (build_cancellation_sender, build_cancellation_events) = channel::bounded(1);
                 ongoing_build_cancellation_sender = Some(build_cancellation_sender);
                 let build_future = builder::build_target(&self.target, build_cancellation_events);
                 ongoing_build_fuse.set(

src/engine/target_actor/mod.rs
@@ -9,7 +9,7 @@ use crate::domain::{Target, TargetId};
 use crate::TerminationMessage;
 use aggregate_target_actor::AggregateTargetActor;
 use anyhow::{Error, Result};
-use async_std::sync::{self, Sender};
+use async_std::channel::{self, Sender};
 use async_std::task::{self, JoinHandle};
 use build_target_actor::BuildTargetActor;
 use service_target_actor::ServiceTargetActor;
@@ -84,10 +84,10 @@ pub fn launch_target_actor(
     watch_option: WatchOption,
     target_actor_output_sender: Sender<TargetActorOutputMessage>,
 ) -> Result<(JoinHandle<()>, TargetActorHandleSet)> {
-    let (termination_sender, termination_events) = sync::channel(1);
-    let (target_invalidated_sender, target_invalidated_events) = sync::channel(1);
+    let (termination_sender, termination_events) = channel::bounded(1);
+    let (target_invalidated_sender, target_invalidated_events) = channel::bounded(1);
     let (target_actor_input_sender, target_actor_input_receiver) =
-        sync::channel(crate::DEFAULT_CHANNEL_CAP);
+        channel::bounded(crate::DEFAULT_CHANNEL_CAP);
 
     let watcher = match watch_option {
         WatchOption::Enabled => {

commit fc881730bfebf5d18fc9b97015c8fd0f26e94d2a

Do not panic when sending on closed channel
Issue introduced in commit ddb3fd6b6b44f12080f0b3a75976b403423e18de

src/engine/target_actor/target_actor_helper.rs
@@ -78,14 +78,14 @@ impl TargetActorHelper {
     pub async fn notify_execution_failed(&mut self, e: Error) {
         self.executed = false;
         let msg = TargetActorOutputMessage::TargetExecutionError(self.target_id.clone(), e);
-        self.target_actor_output_sender.send(msg).await.unwrap()
+        let _ = self.target_actor_output_sender.send(msg).await;
     }
 
     pub async fn send_to_actor(&self, dest: ActorId, msg: ActorInputMessage) {
-        self.target_actor_output_sender
+        let _ = self
+            .target_actor_output_sender
             .send(TargetActorOutputMessage::MessageActor { dest, msg })
-            .await
-            .unwrap()
+            .await;
     }
 
     pub async fn send_to_dependencies(&self, msg: ActorInputMessage) {

src/engine/target_actors.rs
@@ -52,11 +52,11 @@ impl TargetActors {
     }
 
     pub async fn send(&mut self, target_id: &TargetId, msg: ActorInputMessage) -> Result<()> {
-        self.get_target_actor_handles(target_id)?
+        let _ = self
+            .get_target_actor_handles(target_id)?
             .target_actor_input_sender
             .send(msg)
-            .await
-            .unwrap();
+            .await;
 
         Ok(())
     }
@@ -68,11 +68,7 @@ impl TargetActors {
                 kind,
                 requester: ActorId::Root,
             };
-            handles
-                .target_actor_input_sender
-                .send(build_msg)
-                .await
-                .unwrap();
+            let _ = handles.target_actor_input_sender.send(build_msg).await;
         }
 
         Ok(())
@@ -88,11 +84,7 @@ impl TargetActors {
     ) {
         log::debug!("Terminating all targets");
         for handles in target_actor_handles.values() {
-            handles
-                .termination_sender
-                .send(TerminationMessage)
-                .await
-                .unwrap();
+            let _ = handles.termination_sender.send(TerminationMessage).await;
         }
     }
 }

src/main.rs
@@ -121,7 +121,7 @@ fn terminate_on_ctrlc() -> Result<Receiver<TerminationMessage>> {
     task::spawn(async move {
         ctrlc.await;
         log::debug!("Ctrl-C received");
-        termination_sender.send(TerminationMessage).await.unwrap();
+        let _ = termination_sender.send(TerminationMessage).await;
     });
 
     Ok(termination_events)