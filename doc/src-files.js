66080846fb1d7efdfba6f4dd1687b996c40ed2dc
fbecart <florent.becart@gmail.com>
2024-03-20 07:36:30 +0000

    Deploying to gh-pages from  @ b22210bc2d55c3eea3a89cef53f29906607ce063 🚀

--- doc/src-files.js
+++ doc/src-files.js
@@ -1,4 +1,4 @@
-var srcIndex = JSON.parse('{\
-"zinoma":["",[["config",[["yaml",[],["mod.rs","schema.rs"]]],["ir.rs","mod.rs"]],["engine",[["incremental",[["resources_state",[],["cmd_stdout.rs","fs.rs","mod.rs"]]],["mod.rs","storage.rs"]],["target_actor",[],["aggregate_target_actor.rs","build_target_actor.rs","mod.rs","service_target_actor.rs","target_actor_helper.rs"]]],["builder.rs","mod.rs","target_actors.rs","watcher.rs"]]],["async_utils.rs","clean.rs","cli.rs","domain.rs","fs.rs","main.rs","run_script.rs","work_dir.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["zinoma",["",[["config",[["yaml",[],["mod.rs","schema.rs"]]],["ir.rs","mod.rs"]],["engine",[["incremental",[["resources_state",[],["cmd_stdout.rs","fs.rs","mod.rs"]]],["mod.rs","storage.rs"]],["target_actor",[],["aggregate_target_actor.rs","build_target_actor.rs","mod.rs","service_target_actor.rs","target_actor_helper.rs"]]],["builder.rs","mod.rs","target_actors.rs","watcher.rs"]]],["async_utils.rs","clean.rs","cli.rs","domain.rs","fs.rs","main.rs","run_script.rs","work_dir.rs"]]]\
+]'));
 createSrcSidebar();
